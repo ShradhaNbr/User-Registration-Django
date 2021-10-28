68f7171ddad177a754bd789a16aea20584570a1e
ShradhaNbr <sradha.nambiar@gmail.com>
2021-10-28 11:34:51 +0530

    Added : Code for note app using jwt

--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+import jwt
 from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
@@ -5,6 +6,8 @@ from rest_framework.response import Response
 from .models import Notes
 from .serializer import NotesSerializer
 from rest_framework.views import APIView
+import jwt
+from user.views import RegisterLogin
 
 
 # Create your views here.
@@ -16,8 +19,12 @@ class NoteList(APIView):
         """
         This function creates a new note
         """
+        decode_token = request.META.get('HTTP_AUTHORIZATION')
+        decoded_token = jwt.decode(decode_token, 'secret', algorithms="HS256")
+        notes_data = request.data
+        notes_data["user_id"] = decoded_token["id"]
         try:
-            serializer = NotesSerializer(data=request.data)
+            serializer = NotesSerializer(data=notes_data)
             if serializer.is_valid():
                 serializer.save()
                 response = {"message": "Notes added successfully"}
@@ -25,43 +32,59 @@ class NoteList(APIView):
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception:
-            return Response("Invalid data")
+            return Response({
+                "message": "Invalid data"})
 
     def get(self, request):
-        """
-        This function gets particular note when id is passed
-        """
+
+        decode_token = request.META.get('HTTP_AUTHORIZATION')
+        decoded_token = jwt.decode(decode_token, 'secret', algorithms="HS256")
+        user_id = decoded_token["id"]
         try:
-            serializer = NotesSerializer(Notes.objects.filter(user_id=request.data["user_id"]), many=True)
-            return Response(serializer.data)
+            serializer = NotesSerializer(Notes.objects.filter(user_id=user_id), many=True)
+            notesdata = {
+                "message": "Notes for user",
+                "data": serializer.data
+            }
+            return Response(notesdata)
         except Exception:
-            return Response("User not found")
+            return Response({
+                "message": "user not found"})
 
     def put(self, request):
+
         """
         This function updates particular note when id is passed
         """
+        decode_token = request.META.get('HTTP_AUTHORIZATION')
+        decoded_token = jwt.decode(decode_token, 'secret', algorithms="HS256")
+        notes_data = request.data
+        notes_data["user_id"] = decoded_token["id"]
         try:
             note = Notes.objects.get(id=request.data["id"])
-            serializer = NotesSerializer(note, data=request.data)
+            serializer = NotesSerializer(note, data=notes_data)
             if serializer.is_valid():
                 serializer.save()
-                return Response(
-                    {
-                        "message": "updated successfully",
-                        "data": serializer.data
-                    })
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                response = {"message": "Notes added successfully"}
+                return Response(response)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception:
-            return Response("id not found")
+            return Response({
+                "message": "id not found"})
 
     def delete(self, request):
         """
         This function deletes particular notes when id is passed
         """
+        decode_token = request.META.get('HTTP_AUTHORIZATION')
+        decoded_token = jwt.decode(decode_token, 'secret', algorithms="HS256")
+        notes_data = request.data
+        notes_data["user_id"] = decoded_token["id"]
         try:
             note = Notes.objects.get(id=request.data["id"])
             note.delete()
             return Response({"message": "Deleted successfully"})
         except Exception:
-            return Response("id not found")
+            return Response({
+                "message": "id not found"})
